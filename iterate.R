c3b4a2551949da0d388294478e295b59c2c57d2e
alexcengler <alexcengler@gmail.com>
2018-08-02 15:50:41 -0500

    Made first code changes

--- iterate.R
+++ iterate.R
@@ -1,16 +1,18 @@
 library(sf)
 library(tidyverse)
 library(tidycensus)
-library(ggplot2)
 
 chicago_gdf <- st_read("chicago_tracts_2010.shp")
 commarea <- as.character(chicago_gdf$commarea)
+
+## This is creating the parameter reports:
 
 reports <- tibble(
   output_file = stringr::str_c("output/maps-commarea_", commarea, ".html"),
   params = map(commarea, ~list(commarea = .))
 )
 
-head(reports)
+## Look at reports:
+glimpse(reports)
 
 reports %>% pwalk(rmarkdown::render, input = "geospatial.rmd")
